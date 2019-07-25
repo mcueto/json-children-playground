d42ae5897af2eedba1b1873dbaf5e8e40609cb6d
Marcelo Cueto <marcelo.cueto@uchile.cl>
2019-07-25 15:17:11 -0400

    Add vue-jsoneditor

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
     <div class="container">
       <div class="row">
 
-        <div class="col-6 bg-dark text-white">
-          JSON
+          <v-jsoneditor v-model="json"
+                        @input="jsonChanged">
+          </v-jsoneditor>
         </div>
 
         <div class="col-6">
@@ -26,8 +27,22 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    VJsoneditor,
+  data() {
+    return {
+      json: {
+        sucess: true,
+        title: 'Title',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+    };
+  },
+  methods: {
+    jsonChanged(value) {
+      console.log(JSON.stringify(value));
+    },
+  },
 }
 </script>
 
